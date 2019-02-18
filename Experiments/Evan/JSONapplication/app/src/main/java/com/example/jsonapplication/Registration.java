01322ee6885c1a594602f3ad0b9cfc2a6fb9c4ac
Evan G Timmons <evangeorgetimmons@gmail.com>
2019-02-18 17:07:56 -0600

    Fleshed out the registration page code

--- Experiments/Evan/JSONapplication/app/src/main/java/com/example/jsonapplication/Registration.java
+++ Experiments/Evan/JSONapplication/app/src/main/java/com/example/jsonapplication/Registration.java
@@ -22,6 +22,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -69,50 +70,107 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
             case R.id.buttonRegister:
                 RequestQueue queue = Volley.newRequestQueue(this);  // this = context
 
-                EditText IDEdit = (EditText)findViewById(R.id.textID);
-                final String IDInput = IDEdit.getText().toString();
-
                 EditText nameEdit = (EditText)findViewById(R.id.textName);
                 final String nameInput = nameEdit.getText().toString();
 
-                EditText descriptionEdit = (EditText)findViewById(R.id.textDescription);
-                final String descriptionInput = nameEdit.getText().toString();
+                EditText IDEdit = (EditText)findViewById(R.id.textID);
+                final String IDInput = IDEdit.getText().toString();
 
-                Map<String, String> params = new HashMap();
-                params.put("name", "test3");
-                params.put("netid", "test3");
-                params.put("password", "test3");
+                EditText passwordEdit = (EditText)findViewById(R.id.textPassword);
+                final String passwordInput = passwordEdit.getText().toString();
 
-                JSONObject parameters = new JSONObject(params);
-
-                String url = "http://cs309-pp-4.misc.iastate.edu:8080/usersid";
-                //String url = "https://0ea88006-bc29-40d9-8155-873d2ed83f3c.mock.pstmn.io/registration";
-                JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, url, parameters,
-                        new Response.Listener<JSONObject>()
-                        {
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, mJSONURLString, null,
+                        new Response.Listener<JSONObject>() {
                             @Override
                             public void onResponse(JSONObject response) {
-                                // response
-                                //Log.d("Response", response);
-                                Toast.makeText(getApplicationContext(), "Volley success " + response, Toast.LENGTH_LONG).show();
+                                int alreadyTaken = 0;
+                                // Do something with response
+                                try {
+                                    // Get JSON object
+                                    JSONArray array = response.getJSONArray("student");
+
+                                    //Change upper bound of for loop to array.length() to print all values
+                                    for (int i = 0; i < array.length(); i++) {
+                                        // Get current json object
+                                        JSONObject student = array.getJSONObject(i);
+
+                                        // Get the current student (json object) data
+                                        String name = student.getString("name");
+                                        String netid = student.getString("netid");
+                                        String password = student.getString("password");
+
+                                        if(netid.equals(IDInput)){
+                                            Toast.makeText(getApplicationContext(), "NetID Already Registered", Toast.LENGTH_LONG).show();
+                                            alreadyTaken = 1;
+                                        }
+                                    }
+                                    if(alreadyTaken == 0) {
+                                        postData();
+                                    }
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
                             }
                         },
-                        new Response.ErrorListener()
-                        {
+                        new Response.ErrorListener() {
                             @Override
                             public void onErrorResponse(VolleyError error) {
-                                // error
-                                //Log.d("Error.Response", error);
                                 Toast.makeText(getApplicationContext(), "Volley error " + error.getMessage(), Toast.LENGTH_LONG).show();
                             }
                         }
                 );
-                queue.add(postRequest);
+                // Add JsonObjectRequest to the RequestQueue
+                queue.add(jsonObjectRequest);
 
-                //startActivity(new Intent(Registration.this, RegistrationSuccess.class));
                 break;
-
+            }
         }
 
+    public void postData(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        EditText nameEdit = (EditText)findViewById(R.id.textName);
+        final String nameInput = nameEdit.getText().toString();
+
+        EditText IDEdit = (EditText)findViewById(R.id.textID);
+        final String IDInput = IDEdit.getText().toString();
+
+        EditText passwordEdit = (EditText)findViewById(R.id.textPassword);
+        final String passwordInput = passwordEdit.getText().toString();
+
+        Map<String, String> params = new HashMap();
+        params.put("name", nameInput);
+        params.put("netid", IDInput);
+        params.put("password", passwordInput);
+
+        JSONObject parameters = new JSONObject(params);
+
+        String url = "http://cs309-pp-4.misc.iastate.edu:8080/usersid";
+        //String url = "https://0ea88006-bc29-40d9-8155-873d2ed83f3c.mock.pstmn.io/registration";
+        JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, url, parameters,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // response
+                        //Log.d("Response", response);
+                        //Toast.makeText(getApplicationContext(), "Volley success " + response, Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(Registration.this, RegistrationSuccess.class));
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        //Log.d("Error.Response", error);
+                        //Toast.makeText(getApplicationContext(), "Volley error " + error.getMessage(), Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(Registration.this, RegistrationSuccess.class));
+                    }
+                }
+        );
+        queue.add(postRequest);
     }
+
 }

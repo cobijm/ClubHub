3aa52732ffdd0a21e5390faac71153d84de2f77d
scott <svlasic@iastate.edu>
2019-02-18 11:56:05 -0600

    Working login page with GET

--- Experiments/Evan/JSONapplication/app/src/main/java/com/example/jsonapplication/Login.java
+++ Experiments/Evan/JSONapplication/app/src/main/java/com/example/jsonapplication/Login.java
@@ -11,13 +11,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -47,48 +53,92 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
             case R.id.btnLogin:
                 RequestQueue queue = Volley.newRequestQueue(this);  // this = context
 
-                EditText IDEdit = (EditText)findViewById(R.id.loginText);
-                final String IDInput = IDEdit.getText().toString();
+                EditText netIDEdit = (EditText)findViewById(R.id.loginText);
+                final String netIDInput = netIDEdit.getText().toString();
 
-                EditText nameEdit = (EditText)findViewById(R.id.passText);
-                final String nameInput = nameEdit.getText().toString();
+                EditText passEdit = (EditText)findViewById(R.id.passText);
+                final String passInput = passEdit.getText().toString();
 
-                EditText descriptionEdit = (EditText)findViewById(R.id.textDescription);
-                final String descriptionInput = nameEdit.getText().toString();
 
-                String url = "https://0ea88006-bc29-40d9-8155-873d2ed83f3c.mock.pstmn.io/registration";
-                StringRequest postRequest = new StringRequest(Request.Method.POST, url,
-                        new Response.Listener<String>()
-                        {
+                //Initialize a new RequestQueue instance
+                RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, mJSONURLString, null,
+                        new Response.Listener<JSONObject>() {
                             @Override
-                            public void onResponse(String response) {
-                                // response
-                                Log.d("Response", response);
+                            public void onResponse(JSONObject response) {
+                                // Do something with response
+                                try {
+                                    // Get JSON object
+                                    JSONArray array = response.getJSONArray("student");
+
+                                    //Change upper bound of for loop to array.length() to print all values
+                                    for (int i = 0; i < array.length(); i++) {
+                                        // Get current json object
+                                        JSONObject student = array.getJSONObject(i);
+
+                                        // Get the current student (json object) data
+                                        String netid = student.getString("netid");
+                                        String password = student.getString("password");
+
+                                        Toast.makeText(getApplicationContext(), netid + password, Toast.LENGTH_LONG).show();
+
+                                        if(netid.equals(netIDInput) && password.equals(passInput)){
+                                            startActivity(new Intent(Login.this, LoginSuccess.class));
+                                        }
+
+                                    }
+
+                                   // Toast.makeText(getApplicationContext(), "Login Failed ", Toast.LENGTH_LONG).show();
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+
+                                }
                             }
                         },
-                        new Response.ErrorListener()
-                        {
+                        new Response.ErrorListener() {
                             @Override
                             public void onErrorResponse(VolleyError error) {
-                                // error
-                                //Log.d("Error.Response", error);
+                                Toast.makeText(getApplicationContext(), "Volley error " + error.getMessage(), Toast.LENGTH_LONG).show();
                             }
                         }
-                ) {
-                    @Override
-                    protected Map<String, String> getParams()
-                    {
-                        Map<String, String>  params = new HashMap<String, String>();
-                        params.put("id", IDInput);
-                        params.put("name", nameInput);
-                        params.put("description", descriptionInput);
+                );
+//                // Add JsonObjectRequest to the RequestQueue
+//                requestQueue.add(jsonObjectRequest);
+//                String url = "https://0ea88006-bc29-40d9-8155-873d2ed83f3c.mock.pstmn.io/registration";
+//                StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+//                        new Response.Listener<String>()
+//                        {
+//                            @Override
+//                            public void onResponse(String response) {
+//                                // response
+//                                Log.d("Response", response);
+//                            }
+//                        },
+//                        new Response.ErrorListener()
+//                        {
+//                            @Override
+//                            public void onErrorResponse(VolleyError error) {
+//                                // error
+//                                //Log.d("Error.Response", error);
+//                            }
+//                        }
+//                ) {
+//                    @Override
+//                    protected Map<String, String> getParams()
+//                    {
+//                        Map<String, String>  params = new HashMap<String, String>();
+//                        params.put("id", IDInput);
+//                        params.put("name", nameInput);
+//                        params.put("description", descriptionInput);
+//
+//                        return params;
+//                    }
+//                };
+//                queue.add(postRequest);
 
-                        return params;
-                    }
-                };
-                queue.add(postRequest);
-
-                startActivity(new Intent(Login.this, LoginSuccess.class));
+                requestQueue.add(jsonObjectRequest);
                 break;
 
         }

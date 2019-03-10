76bc9ad54780a53f9a854bec2ee1862ad5449d20
scott <svlasic@iastate.edu>
2019-03-10 18:23:35 -0500

    After clicking on Club in ListView it takes you to the correctly titled club home page

--- Frontend/ClubHub/app/src/main/java/com/example/ClubHub/UserHomePage.java
+++ Frontend/ClubHub/app/src/main/java/com/example/ClubHub/UserHomePage.java
@@ -2,6 +2,7 @@ package com.example.ClubHub;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -65,13 +66,15 @@ public class UserHomePage extends Activity {
                         public void onItemClick(AdapterView<?> parent, View view,
                                                 int position, long id) {
 
+
                             // ListView Clicked item index
                             int itemPosition     = position;
 
                             // ListView Clicked item value
                             String  itemValue    = (String) listView.getItemAtPosition(position);
 
-                            Intent intent = new Intent(getApplicationContext(), ClubHomePage.class);
+                            Intent intent = new Intent(UserHomePage.this, ClubHomePage.class);
+                            intent.putExtra("clubName", clubNames.get(position));
                             startActivity(intent);
 
                         }

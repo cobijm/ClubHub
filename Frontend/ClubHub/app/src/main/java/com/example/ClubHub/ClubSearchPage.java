0933d722c7d9b52a232baaa8c324f2fbc897762d
scott <svlasic@iastate.edu>
2019-03-09 18:54:10 -0600

    Formatting club search page

--- Frontend/ClubHub/app/src/main/java/com/example/ClubHub/ClubSearchPage.java
+++ Frontend/ClubHub/app/src/main/java/com/example/ClubHub/ClubSearchPage.java
@@ -1,47 +1,57 @@
 package com.example.ClubHub;
 
-import android.app.ListActivity;
-import android.support.constraint.ConstraintSet;
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
+import android.text.TextUtils;
+import android.view.Window;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.SearchView;
 
-import java.util.ArrayList;
+/**
+ * Shows a list that can be filtered in-place with a SearchView in non-iconified mode.
+ */
+public class ClubSearchPage extends Activity implements SearchView.OnQueryTextListener {
 
-public class ClubSearchPage extends ListActivity {
-    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
-    ArrayList<String> listItems=new ArrayList<String>();
+    private SearchView mSearchView;
+    private ListView mListView;
 
-    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
-    ArrayAdapter<String> adapter;
-
-    //RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
-    int clickCounter=0;
+    private final String[] mStrings = { "Google", "Apple", "Samsung", "Sony", "LG", "HTC" };
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+
         setContentView(R.layout.activity_club_search_page);
-        adapter=new ArrayAdapter<String>(this,
+
+        mSearchView = (SearchView) findViewById(R.id.search_view);
+        mListView = (ListView) findViewById(R.id.list_view);
+        mListView.setAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
-                listItems);
-        setListAdapter(adapter);
+                mStrings));
+        mListView.setTextFilterEnabled(true);
+        setupSearchView();
     }
 
-    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
-    public void addItems(View v) {
-        listItems.add("Clicked : "+clickCounter++);
-        adapter.notifyDataSetChanged();
+    private void setupSearchView() {
+        mSearchView.setIconifiedByDefault(false);
+        mSearchView.setOnQueryTextListener(this);
+        mSearchView.setSubmitButtonEnabled(true);
+        mSearchView.setQueryHint("Search Here");
     }
 
+    public boolean onQueryTextChange(String newText) {
+        if (TextUtils.isEmpty(newText)) {
+            mListView.clearTextFilter();
+        } else {
+            mListView.setFilterText(newText.toString());
+        }
+        return true;
+    }
+
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
 }

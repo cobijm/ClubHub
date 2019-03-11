0a30156fbc1a0fb4380991a3e21fd67fac9d38d9
scott <svlasic@iastate.edu>
2019-03-11 11:32:37 -0500

    Comments for Login Page

--- Frontend/ClubHub/app/src/main/java/com/example/ClubHub/UserHomePage.java
+++ Frontend/ClubHub/app/src/main/java/com/example/ClubHub/UserHomePage.java
@@ -50,7 +50,7 @@ public class UserHomePage extends Activity {
                 try {
 
                     //Maybe to change
-                    JSONArray array = response.getJSONArray("enrollment");
+                    JSONArray array = response.getJSONArray("enrollments");
                     for (int i = 0; i < array.length(); i++) {
 
                         JSONObject clubEnrollment = array.getJSONObject(i);

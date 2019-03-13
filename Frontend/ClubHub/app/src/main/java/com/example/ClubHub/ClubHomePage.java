c310aa60fcdeab35a69660d4cd4b7c8194d16563
Evan G Timmons <evangeorgetimmons@gmail.com>
2019-03-13 09:33:42 -0500

    Added a hyperlink domain to the club home page

--- Frontend/ClubHub/app/src/main/java/com/example/ClubHub/ClubHomePage.java
+++ Frontend/ClubHub/app/src/main/java/com/example/ClubHub/ClubHomePage.java
@@ -3,6 +3,8 @@ package com.example.ClubHub;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -36,8 +38,14 @@ public class ClubHomePage extends AppCompatActivity {
         TextView myTxt = (TextView) findViewById(R.id.clubName);
         myTxt.setText(clubName);
 
-        TextView myTxt2 = (TextView)findViewById(R.id.domain);
-        myTxt2.setText(domainName);
+        //TextView myTxt2 = (TextView)findViewById(R.id.domain);
+        //myTxt2.setText(domainName);
+
+        TextView myTxt2 =(TextView)findViewById(R.id.domain);
+        myTxt2.setClickable(true);
+        myTxt2.setMovementMethod(LinkMovementMethod.getInstance());
+        String text = "<a href='http://www." + domainName + "'> "+ domainName + " </a>";
+        myTxt2.setText(Html.fromHtml(text));
 
         TextView myTxt3 = (TextView)findViewById(R.id.status);
         myTxt3.setText(clubStatus);

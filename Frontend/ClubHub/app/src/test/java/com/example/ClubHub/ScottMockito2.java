bb0822c9e160204c82c1e560515d39ada35f1f00
Scott <svlasic247@gmail.com>
2019-04-08 16:41:00 -0500

    Separated the code into 3 tests instead of 1 giant one

--- Frontend/ClubHub/app/src/test/java/com/example/ClubHub/ScottMockito2.java
+++ Frontend/ClubHub/app/src/test/java/com/example/ClubHub/ScottMockito2.java
@@ -1,6 +1,27 @@
 package com.example.ClubHub;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class ScottMockito2 {
 
+    @Test
+    public void loginTest(){
 
+        Login tester = mock(Login.class);
+
+        ArrayList<String> list = new ArrayList<>();
+
+        String user = "svlasic";
+        String pass = "1234";
+
+        when(tester.loginCredentials(user,pass)).thenReturn(list);
+
+        Assert.assertEquals(tester.loginCredentials(user,pass), list);
+    }
 }
